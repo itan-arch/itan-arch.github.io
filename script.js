491d257ae2904fa636d406de5d1c485fbb017370
itan-arch <angelicazaragozar1@gmail.com>
2024-12-08 02:13:41 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function mostrarSeccion(id) {
     const secciones = document.querySelectorAll('.contenido');
-    secciones.forEach(seccion => seccion.classList.add('oculto'));
-    document.getElementById(id).classList.remove('oculto');
+    secciones.forEach(seccion => seccion.classList.add('oculto'));  // Oculta todas las secciones
+    document.getElementById(id).classList.remove('oculto');  // Muestra la sección seleccionada
 }
-
